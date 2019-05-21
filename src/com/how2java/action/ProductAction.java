d48929b9b1db36d1a036283527cc97f0b4f1e1ea
陶文猛 <15152806231@163.com>
2019-05-21 14:10:27 +0800

    iterator标签

--- src/com/how2java/action/ProductAction.java
+++ src/com/how2java/action/ProductAction.java
@@ -1,5 +1,7 @@
 package com.how2java.action;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import com.how2java.bean.Product;
@@ -8,8 +10,19 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction extends ActionSupport{
     private Product product;
- 
-    public String show() {
+    private List<Product> products;
+    
+    
+    
+    public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public String show() {
     	
         product = new Product();
         product.setName("iphone7");
@@ -23,6 +36,28 @@ public class ProductAction extends ActionSupport{
     	System.out.println("product.name:" + product.getName());
     	System.out.println("product.age:" + product.getAge());
     	return "show";
+    }
+    
+    public String list(){
+    	products = new ArrayList();
+    	
+    	Product p1 = new Product();
+    	p1.setId(1);
+    	p1.setName("product1");
+    	
+    	Product p2 = new Product();
+    	p2.setId(2);
+    	p2.setName("product2");
+    	
+    	Product p3 = new Product();
+    	p3.setId(3);
+    	p3.setName("product3");
+    	
+    	products.add(p1);
+    	products.add(p2);
+    	products.add(p3);
+    	
+    	return "list";
     }
  
     public Product getProduct() {
